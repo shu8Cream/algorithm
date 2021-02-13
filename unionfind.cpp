7d541300d9a8a9cb29f1453c5073766f633d7937
shu8Cream <shu.vgage@gmail.com>
2021-02-13 16:05:49 +0900

    add unionfind

---
+++ unionfind.cpp
@@ -0,0 +1,44 @@
+//
+// Union Find(マージテク)
+//
+/*
+
+    マージテク（データ構造をマージする一般的なテク） O(NlogN^2)
+
+*/
+#include <bits/stdc++.h>
+using namespace std;
+#define rep(i,n) for (int i=0; i<(n); i++)
+using ll = long long;
+using P = pair<int, int>;
+using vi = vector<int>;
+using vvi = vector<vi>;
+using Graph = vvi;
+
+struct UnionFind{
+    vector<int> par;
+    vector<map<int, int>> mp;
+    UnionFind(int n=0): par(n, -1), mp(n) {}
+    int find(int x){
+        if(par[x] < 0) return x;
+        return par[x] = find(par[x]);
+    }
+    bool unite(int x, int y){
+        x = find(x); y = find(y);
+        if(x == y) return false;
+        if(par[x] > par[y]) swap(x,y); //マージテク
+        for(auto p : mp[y]){
+            mp[x][p.first] += p.second;
+        }
+        mp[y] = map<int,int>(); //メモリの解放
+        par[x] += par[y];
+        par[y] = x;
+        return true;
+    }
+    bool same(int x, int y) {return find(x) == find(y);}
+    int size(int x) {return -par[find(x)];}
+};
+
+int main(){
+
+}
