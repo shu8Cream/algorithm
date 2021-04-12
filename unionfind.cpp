1f6c51292ff27d21718736fe201b2ade99f7be76
shu8Cream <shu.vgage@gmail.com>
2021-04-12 12:50:41 +0900

    fix detail

--- unionfind.cpp
+++ unionfind.cpp
@@ -41,5 +41,17 @@ struct UnionFind{
 };
 
 int main(){
-
+    int n,q;
+    cin >> n >> q;
+    UnionFind uf(n);
+    rep(_,q){
+        int t,u,v;
+        cin >> t >> u >> v;
+        if(t){
+            if(uf.same(u,v)) cout << 1 << endl;
+            else cout << 0 << endl;
+        }else{
+            uf.merge(u,v);
+        }
+    }
 }
