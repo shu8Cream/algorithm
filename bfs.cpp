20d9bfad449761cb21c1d9e529a37c9b4a365226
shu8Cream <shu.vgage@gmail.com>
2021-04-12 12:49:07 +0900

    fix detail

--- bfs.cpp
+++ bfs.cpp
@@ -6,48 +6,40 @@
     基本バージョン
 
 */
-#include <iostream>
-#include <vector>
-#include <queue>
+#include <bits/stdc++.h>
 using namespace std;
-using Graph = vector<vector<int>>;
+#define rep(i,n) for (int i=0; i<(n); i++)
+using ll = long long;
+using P = pair<int, int>;
+using vi = vector<int>;
+using vvi = vector<vi>;
 
 int main() {
-    // 頂点数と辺数
-    int N, M; cin >> N >> M;
+    int n, m; cin >> n >> m;
 
-    // グラフ入力受取 (ここでは無向グラフを想定)
-    Graph G(N);
-    for (int i = 0; i < M; ++i) {
+    vvi G(n);
+    for (int i = 0; i < m; ++i) {
         int a, b;
         cin >> a >> b;
         G[a].push_back(b);
         G[b].push_back(a);
     }
 
-    // BFS のためのデータ構造
-    vector<int> dist(N, -1); // 全頂点を「未訪問」に初期化
-    queue<int> que;
+    vector<int> dist(n, -1);
+    queue<int> q;
 
-    // 初期条件 (頂点 0 を初期ノードとする)
     dist[0] = 0;
-    que.push(0); // 0 を橙色頂点にする
+    q.push(0);
 
-    // BFS 開始 (キューが空になるまで探索を行う)
-    while (!que.empty()) {
-        int v = que.front(); // キューから先頭頂点を取り出す
-        que.pop();
+    while (!q.empty()) {
+        int v = q.front(); q.pop();
 
-        // v から辿れる頂点をすべて調べる
         for (int nv : G[v]) {
-            if (dist[nv] != -1) continue; // すでに発見済みの頂点は探索しない
-
-            // 新たな白色頂点 nv について距離情報を更新してキューに追加する
+            if (dist[nv] != -1) continue;
             dist[nv] = dist[v] + 1;
-            que.push(nv);
+            q.push(nv);
         }
     }
 
-    // 結果出力 (各頂点の頂点 0 からの距離を見る)
-    for (int v = 0; v < N; ++v) cout << v << ": " << dist[v] << endl;
+    for (int v = 0; v < n; ++v) cout << v << ": " << dist[v] << endl;
 }
