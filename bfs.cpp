f7f2c9e7070ae88f3d0526ddf151017c88baa5ca
shu8Cream <shu.vgage@gmail.com>
2021-11-07 14:38:03 +0900

    経路復元を追加

--- bfs.cpp
+++ bfs.cpp
@@ -4,12 +4,11 @@
 /*
 
     基本バージョン
+    関数バージョン追加
+    経路復元追加　20211107
+        直前のノードを持つ配列prevを利用する
 
 */
-/**
-*    author:  shu8Cream
-*    created: 16.09.2021 23:06:22
-**/
 
 #include <bits/stdc++.h>
 using namespace std;
@@ -55,13 +54,14 @@ int main() {
 
     vvi G(n);
     for (int i = 0; i < m; ++i) {
-        int a, b;
-        cin >> a >> b;
+        int a, b; cin >> a >> b;
+        a--; b--;
         G[a].push_back(b);
         G[b].push_back(a);
     }
 
-    vector<int> dist(n, -1);
+    vi dist(n, -1);
+    vi prev(n, -1);
     queue<int> q;
     dist[0] = 0;
     q.push(0);
@@ -71,10 +71,23 @@ int main() {
         for (int nv : G[v]) {
             if (dist[nv] != -1) continue;
             dist[nv] = dist[v] + 1;
+            prev[nv] = v;
             q.push(nv);
         }
     }
 
     auto a = bfs(0,G);
     for (int v = 0; v < n; ++v) cout << v << ": " << a[v] << endl;
+
+    // 最短経路の経路復元、curにゴールを入れる
+    int cur = 5, start = 0; 
+    vi res;
+    while(cur!=start){
+        res.push_back(cur);
+        cur = prev[cur];
+    }
+    res.push_back(start);
+    reverse(all(res));
+    for(auto p : res) cout << p << " ";
+    cout << endl;
 }
