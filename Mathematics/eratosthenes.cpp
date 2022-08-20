5ef4e66e652029d265eea4a7043c2ef8b43e9650
shu8Cream <shu.vgage@gmail.com>
2022-08-20 15:57:09 +0900

    feat: 約数列挙機能の追加

--- Mathematics/eratosthenes.cpp
+++ Mathematics/eratosthenes.cpp
@@ -9,12 +9,65 @@
 
 #include <bits/stdc++.h>
 using namespace std;
-#define rep(i,n) for (int i=0; i<(n); i++)
+#define overload3(a,b,c,d,...) d
+#define rep1(i,n) for (int i=0; i<(n); i++)
+#define rep2(i,a,n) for (int i=(a); i<(n); i++)
+#define rep(...) overload3(__VA_ARGS__, rep2, rep1)(__VA_ARGS__)
+#define rrep1(i,n) for (int i=(n-1); i>=0; i--)
+#define rrep2(i,a,n) for (int i=(n-1); i>=(a); i--)
+#define rrep(...) overload3(__VA_ARGS__, rrep2, rrep1)(__VA_ARGS__)
 #define all(x) (x).begin(), (x).end()
+#define rall(x) (x).rbegin(), (x).rend()
+#define sz(x) int((x).size())
+#define pcnt __builtin_popcountll
 using ll = long long;
-using P = pair<int,int>;
-using vi = vector<int>;
-using vvi = vector<vi>;
+using P = pair<ll,ll>;
+template<typename T> using vc = vector<T>;
+template<typename T> using vv = vc<vc<T>>;
+using vi = vc<ll>;
+using vvi = vv<ll>;
+const int INF = 1e9;
+const ll LINF = 8e18;
+template<typename T>istream& operator>>(istream&i,vc<T>&v){rep(j,sz(v))i>>v[j];return i;}
+template<typename T>string join(const T&v,const string& d=""){stringstream s;rep(i,sz(v))(i?s<<d:s)<<v[i];return s.str();}
+template<typename T>ostream& operator<<(ostream&o,const vc<T>&v){if(sz(v))o<<join(v," ");return o;}
+template<typename T1,typename T2>istream& operator>>(istream&i,pair<T1,T2>&v){return i>>v.first>>v.second;}
+template<typename T1,typename T2>ostream& operator<<(ostream&o,const pair<T1,T2>&v){return o<<v.first<<","<<v.second;}
+template<class T> inline bool chmax(T& a, T b) {if(a<b) { a=b;return true; } return false;}
+template<class T> inline bool chmin(T& a, T b) {if(a>b) { a=b;return true; } return false;}
+
+template <class T> string to_string(T s);
+template <class S, class T> string to_string(pair<S, T> p);
+string to_string(char c) { return string(1, c); }
+string to_string(string s) { return s; }
+string to_string(const char s[]) { return string(s); }
+
+template <class T>
+string to_string(T v) {
+    if (v.empty()) return "{}";
+    string ret = "{";
+    for (auto x : v) ret += to_string(x) + ",";
+    ret.back() = '}';
+    return ret;
+}
+template <class S, class T>
+string to_string(pair<S, T> p) {
+    return "{" + to_string(p.first) + ":" + to_string(p.second) + "}";
+}
+
+void debug_out() { cout << endl; }
+
+template <typename Head, typename... Tail>
+void debug_out(Head H, Tail... T) {
+    cout << to_string(H) << " ";
+    debug_out(T...);
+}
+
+#ifdef _DEBUG
+#define debug(...) debug_out(__VA_ARGS__)
+#else
+#define debug(...)
+#endif
 
 struct Sieve{
     int n;
@@ -55,20 +108,55 @@ struct Sieve{
         }
         return res;
     }
+
+    // mの約数を列挙、返り値は約数の個数
+    int divisor(ll m, vi& a){
+        ll cnt = 1; a[0] = 1;
+        while(m!=1){
+            ll num=1, i=f[m], temp=i;
+            while(m%i==0){m/=i;num++;}
+            rep(k,1,num){
+                rep(j,0,cnt)a[k*cnt+j]=a[j]*temp;
+                temp*=i;
+            }
+            cnt*=num;
+        }
+        sort(a.begin(),a.begin()+cnt);
+        return cnt;
+    }
 };
+Sieve sieve(15001000);
+
+void YUKI2043(){
+    ll a,b,s;
+    cin >> a >> b >> s;
+    vi div(500);
+    ll ans = 0;
+    rep(i,1,s+1){
+        if(i>a*b) break;
+        int cnt = sieve.divisor(i,div);
+        rep(j,cnt){
+            ll p = div[j];
+            if(a-p+1<0 || b-i/p+1<0) continue;
+            ans += (a-p+1) * (b-i/p+1);
+        }
+    }
+    cout << ans << endl;
+}
 
 int main() {
     cin.tie(nullptr);
     ios::sync_with_stdio(false);
 
-    Sieve sieve(15001000);
-    int n;
-    cin >> n;
-    auto f = sieve.factor(n);
-    auto fl = sieve.factorList(n);
-    for(auto p : f){
-        cout << p.first << " " << p.second << endl;
-    }
+    YUKI2043();
+
+    // int n;
+    // cin >> n;
+    // auto f = sieve.factor(n);
+    // auto fl = sieve.factorList(n);
+    // for(auto p : f){
+    //     cout << p.first << " " << p.second << endl;
+    // }
     // for(auto p : fl){
     //     cout << p << endl;
     // }
