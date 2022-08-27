a62ceec39eedfddab7a130b84d0b32a014e1e2fd
shu8Cream <shu.vgage@gmail.com>
2022-08-27 16:41:20 +0900

    fix: フォーマットの整理、広義単調増加パターンの追加

--- lis.cpp
+++ lis.cpp
@@ -1,49 +1,101 @@
-//
-//  LIS(Longest Increase Subsequence) / 最長増加部分列 
-//
-/*
-
-    二分探索による実装
-    計算量：O(NlogN)
-
-*/
+/**
+ * @file lis.cpp
+ * @author shu8Cream
+ * @brief 
+ * @version 0.1
+ * @date 2022-08-27
+ * 
+ * @verify https://atcoder.jp/contests/abc134/tasks/abc134_e
+ * 
+ */
 
 #include <bits/stdc++.h>
 using namespace std;
-#define rep(i,n) for (int i=0; i<(n); i++)
-#define rrep(i,n) for (int i=(n-1); i>=0; i--)
+#define overload3(a,b,c,d,...) d
+#define rep1(i,n) for (int i=0; i<(n); i++)
+#define rep2(i,a,n) for (int i=(a); i<(n); i++)
+#define rep(...) overload3(__VA_ARGS__, rep2, rep1)(__VA_ARGS__)
+#define rrep1(i,n) for (int i=(n-1); i>=0; i--)
+#define rrep2(i,a,n) for (int i=(n-1); i>=(a); i--)
+#define rrep(...) overload3(__VA_ARGS__, rrep2, rrep1)(__VA_ARGS__)
 #define all(x) (x).begin(), (x).end()
 #define rall(x) (x).rbegin(), (x).rend()
 #define sz(x) int((x).size())
+#define pcnt __builtin_popcountll
 using ll = long long;
 using P = pair<ll,ll>;
-using vi = vector<ll>;
-using vvi = vector<vi>;
-const ll INF = INT64_MAX;
+template<typename T> using vc = vector<T>;
+template<typename T> using vv = vc<vc<T>>;
+using vi = vc<ll>;
+using vvi = vv<ll>;
+const int INF = 1e9;
+const ll LINF = 8e18;
+template<typename T>istream& operator>>(istream&i,vc<T>&v){rep(j,sz(v))i>>v[j];return i;}
+template<typename T>string join(const T&v,const string& d=""){stringstream s;rep(i,sz(v))(i?s<<d:s)<<v[i];return s.str();}
+template<typename T>ostream& operator<<(ostream&o,const vc<T>&v){if(sz(v))o<<join(v," ");return o;}
+template<typename T1,typename T2>istream& operator>>(istream&i,pair<T1,T2>&v){return i>>v.first>>v.second;}
+template<typename T1,typename T2>ostream& operator<<(ostream&o,const pair<T1,T2>&v){return o<<v.first<<","<<v.second;}
+template<class T> inline bool chmax(T& a, T b) {if(a<b) { a=b;return true; } return false;}
+template<class T> inline bool chmin(T& a, T b) {if(a>b) { a=b;return true; } return false;}
 
-template<class T> inline bool chmax(T& a, T b) {
-    if (a < b) { a = b; return true; }
-    return false;
+template <class T> string to_string(T s);
+template <class S, class T> string to_string(pair<S, T> p);
+string to_string(char c) { return string(1, c); }
+string to_string(string s) { return s; }
+string to_string(const char s[]) { return string(s); }
+
+template <class T>
+string to_string(T v) {
+    if (v.empty()) return "{}";
+    string ret = "{";
+    for (auto x : v) ret += to_string(x) + ",";
+    ret.back() = '}';
+    return ret;
 }
-template<class T> inline bool chmin(T& a, T b) {
-    if (a > b) { a = b; return true; }
-    return false;
+template <class S, class T>
+string to_string(pair<S, T> p) {
+    return "{" + to_string(p.first) + ":" + to_string(p.second) + "}";
 }
+
+void debug_out() { cout << endl; }
+
+template <typename Head, typename... Tail>
+void debug_out(Head H, Tail... T) {
+    cout << to_string(H) << " ";
+    debug_out(T...);
+}
+
+#ifdef _DEBUG
+#define debug(...) debug_out(__VA_ARGS__)
+#else
+#define debug(...)
+#endif
 
 int LIS(vi &a){
     int n = sz(a);
-    vi dp(n,INF);
+    vi dp(n,LINF);
     rep(i,n){
-        auto itr = lower_bound(all(dp), a[i]);
+        auto itr = lower_bound(all(dp), a[i]); // 狭義単調増加
+        // auto itr = upper_bound(all(dp), a[i]); // 広義単調増加
         *itr = a[i];
     }
-    return lower_bound(all(dp), INF) - dp.begin();
+    return lower_bound(all(dp), LINF) - dp.begin();
+}
+
+void ABC134E(){
+    ll n; cin >> n;
+    vi a(n); cin >> a;
+    vi dp(n,LINF);
+    rrep(i,n){
+        auto itr = upper_bound(all(dp), a[i]);
+        *itr = a[i];
+    }
+    ll ans = lower_bound(all(dp), LINF) - dp.begin();
+    cout << ans << endl;
 }
 
 int main() {
     cin.tie(nullptr);
     ios::sync_with_stdio(false);
-    int n; cin >> n;
-    vi a(n); rep(i,n) cin >> a[i];
-    cout << LIS(a) << endl;
+    ABC134E();
 }
