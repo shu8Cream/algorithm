ef35133f590920eee82d4e655b5f3fcb6bc301e5
shu8Cream <shu.vgage@gmail.com>
2022-10-30 00:20:04 +0900

    fix: powの実装を再帰からshift演算へ変更

--- Mathematics/mod_int.cpp
+++ Mathematics/mod_int.cpp
@@ -81,11 +81,13 @@ struct mint{
     mint operator-(const mint a) const { return mint(*this) -= a;}
     mint operator*(const mint a) const { return mint(*this) *= a;}
     mint pow(ll t) const {
-        if (!t) return 1;
-        mint a = pow(t>>1);
-        a *= a;
-        if (t&1) a *= *this;
-        return a;
+        mint a = *this, r = 1;
+        while(t){
+            if(t&1) r *= a;
+            a *= a;
+            t >>= 1;
+        }
+        return r;
     }
 
     mint inv() const { return pow(mod-2);}
