10c03f637edf5c082c2a4c9bbdae51d67b24b9b1
shu8Cream <shu.vgage@gmail.com>
2023-03-15 20:53:06 +0900

    fix: 経路復元を整備

--- Graph/bfs.cpp
+++ Graph/bfs.cpp
@@ -1,39 +1,103 @@
-//
-//  BFS（幅優先探索）基本ver.
-//
-/*
-
-    基本バージョン
-    関数バージョン追加
-    経路復元追加　20211107
-        直前のノードを持つ配列prevを利用する
-
-*/
+/**
+*    author:  shu8Cream
+*    created: 15.03.2023 20:44:59
+**/
 
 #include <bits/stdc++.h>
 using namespace std;
-#define rep(i,n) for (int i=0; i<(n); i++)
-#define rrep(i,n) for (int i=(n-1); i>=0; i--)
+#define overload3(a,b,c,d,...) d
+#define rep1(i,n) for (int i=0; i<(n); i++)
+#define rep2(i,a,n) for (int i=(a); i<(n); i++)
+#define rep(...) overload3(__VA_ARGS__, rep2, rep1)(__VA_ARGS__)
+#define rrep1(i,n) for (int i=(n-1); i>=0; i--)
+#define rrep2(i,a,n) for (int i=(n-1); i>=(a); i--)
+#define rrep(...) overload3(__VA_ARGS__, rrep2, rrep1)(__VA_ARGS__)
 #define all(x) (x).begin(), (x).end()
 #define rall(x) (x).rbegin(), (x).rend()
 #define sz(x) int((x).size())
+#define pcnt __builtin_popcountll
 using ll = long long;
 using P = pair<ll,ll>;
-using vi = vector<ll>;
-using vvi = vector<vi>;
+template<typename T> using vc = vector<T>;
+template<typename T> using vv = vc<vc<T>>;
+using vi = vc<ll>;
+using vvi = vv<ll>;
+const int INF = 1e9;
+const ll LINF = 8e18;
+template<typename T>istream& operator>>(istream&i,vc<T>&v){rep(j,sz(v))i>>v[j];return i;}
+template<typename T>string join(const T&v,const string& d=""){stringstream s;rep(i,sz(v))(i?s<<d:s)<<v[i];return s.str();}
+template<typename T>ostream& operator<<(ostream&o,const vc<T>&v){if(sz(v))o<<join(v," ");return o;}
+template<typename T1,typename T2>istream& operator>>(istream&i,pair<T1,T2>&v){return i>>v.first>>v.second;}
+template<typename T1,typename T2>ostream& operator<<(ostream&o,const pair<T1,T2>&v){return o<<v.first<<","<<v.second;}
+template<class T> inline bool chmax(T& a, T b) {if(a<b) { a=b;return true; } return false;}
+template<class T> inline bool chmin(T& a, T b) {if(a>b) { a=b;return true; } return false;}
 
-template<class T> inline bool chmax(T& a, T b) {
-    if (a < b) { a = b; return true; }
-    return false;
+// stable sort
+template <typename T>
+vc<int> argsort(const vc<T> &A) {
+    vc<int> ids(sz(A));
+    iota(all(ids), 0);
+    sort(all(ids),
+        [&](int i, int j) { return (A[i] == A[j] ? i < j : A[i] < A[j]); });
+    return ids;
 }
-template<class T> inline bool chmin(T& a, T b) {
-    if (a > b) { a = b; return true; }
-    return false;
+
+// A[I[0]], A[I[1]], ...
+template <typename T>
+vc<T> rearrange(const vc<T> &A, const vc<int> &I) {
+    vc<T> B(sz(I));
+    rep(i, sz(I)) B[i] = A[I[i]];
+    return B;
 }
+
+template <typename T, typename U>
+vc<T> cumsum(vc<U> &A, int off = 1) {
+    vc<T> B(sz(A) + 1);
+    rep(i, sz(A)) B[i + 1] = B[i] + A[i];
+    if (off == 0) B.erase(B.begin());
+    return B;
+}
+
+template <class T> string to_string(T s);
+template <class S, class T> string to_string(pair<S, T> p);
+string to_string(char c) { return string(1, c); }
+string to_string(string s) { return s; }
+string to_string(const char s[]) { return string(s); }
+
+template <class T>
+string to_string(T v) {
+    if (v.empty()) return "{}";
+    string ret = "{";
+    for (auto x : v) ret += to_string(x) + ",";
+    ret.back() = '}';
+    return ret;
+}
+template <class S, class T>
+string to_string(pair<S, T> p) {
+    return "{" + to_string(p.first) + ":" + to_string(p.second) + "}";
+}
+
+void debug_out() { cout << endl; }
+
+template <typename Head, typename... Tail>
+void debug_out(Head H, Tail... T) {
+    cout << to_string(H) << " ";
+    debug_out(T...);
+}
+
+#ifdef _DEBUG
+#define debug(...) debug_out(__VA_ARGS__)
+#else
+#define debug(...)
+#endif
+
+const int MX_SIZE = 200200;
+int pre[MX_SIZE];
 
 vi bfs(int s, vvi &to){
     int n = sz(to);
     vi dist(n, -1);
+    rep(i,n) pre[i] = -1;
     queue<int> q;
     dist[s] = 0;
     q.push(s);
@@ -43,51 +107,40 @@ vi bfs(int s, vvi &to){
         for (int nv : to[v]) {
             if (dist[nv] != -1) continue;
             dist[nv] = dist[v] + 1;
+            pre[nv] = v;
             q.push(nv);
         }
     }
     return dist;
 }
 
-int main() {
-    int n, m; cin >> n >> m;
+vi restore(int t){
+    vi path;
+    while(t!=-1){
+        path.push_back(t);
+        t = pre[t];
+    }
+    reverse(all(path));
+    return path;
+}
 
-    vvi G(n);
-    for (int i = 0; i < m; ++i) {
-        int a, b; cin >> a >> b;
+void ABC168D(){
+    int n,m; cin >> n >> m;
+    vvi to(n);
+    rep(i,m){
+        int a,b; cin >> a >> b;
         a--; b--;
-        G[a].push_back(b);
-        G[b].push_back(a);
+        to[a].push_back(b);
+        to[b].push_back(a);
     }
+    vi d = bfs(0,to);
+    cout<<"Yes"<<endl;
+    rep(i,1,n) cout<<pre[i]+1<<(i==n-1?'\n':' ');
+}
 
-    vi dist(n, -1);
-    vi prev(n, -1);
-    queue<int> q;
-    dist[0] = 0;
-    q.push(0);
-
-    while (!q.empty()) {
-        int v = q.front(); q.pop();
-        for (int nv : G[v]) {
-            if (dist[nv] != -1) continue;
-            dist[nv] = dist[v] + 1;
-            prev[nv] = v;
-            q.push(nv);
-        }
-    }
-
-    auto a = bfs(0,G);
-    for (int v = 0; v < n; ++v) cout << v << ": " << a[v] << endl;
-
-    // 最短経路の経路復元、curにゴールを入れる
-    int cur = 5, start = 0; 
-    vi res;
-    while(cur!=start){
-        res.push_back(cur);
-        cur = prev[cur];
-    }
-    res.push_back(start);
-    reverse(all(res));
-    for(auto p : res) cout << p << " ";
-    cout << endl;
+int main() {
+    cin.tie(nullptr);
+    ios::sync_with_stdio(false);
+    cout << fixed << setprecision(15);
+    ABC168D();
 }
